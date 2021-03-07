5ae1637b16dd3cb5724d2e580bb6aaa5a1be145d
Nursultan Akhmadov <nur-sultan17@mail.ru>
2021-03-07 10:01:26 +0300

    supplemented graph for coin

--- app/src/main/java/com/nursultan/cryptoapp/CoinDetailActivity.kt
+++ app/src/main/java/com/nursultan/cryptoapp/CoinDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -22,7 +23,6 @@ import kotlin.math.roundToInt
 class CoinDetailActivity : AppCompatActivity() {
 
     private lateinit var viewModel: CoinViewModel
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,57 +54,41 @@ class CoinDetailActivity : AppCompatActivity() {
 
         var series = LineGraphSeries<DataPoint>()
 
-       // graphCoinPrice.gridLabelRenderer.numVerticalLabels=5
-       // graphCoinPrice.gridLabelRenderer.numHorizontalLabels=4
+        // graphCoinPrice.gridLabelRenderer.numVerticalLabels=5
+        // graphCoinPrice.gridLabelRenderer.numHorizontalLabels=4
 
         viewModel.getCoinDailyInfo(fromSymbol).observe(this, Observer {
 
-           // if (it.isNotEmpty()) {
+            if (it.isNotEmpty()) {
                 for (di in it) {
                     val ff = dataFormat.format(Date(di.time.toLong() * 1000))
-                    series.appendData(DataPoint(di.time.toDouble(), di.close), true, 7)
+                    series.appendData(DataPoint(di.time.toDouble(), di.close), true, 8)
                 }
-
-            series.appendData(DataPoint(1614447285.0,50000.0), false, 8)
-            series.appendData(DataPoint(1614547285.0,50000.0), false, 8)
-            series.appendData(DataPoint(1614633685.0,55000.0), false, 8)
-            series.appendData(DataPoint(1614720085.0,51000.0), false, 8)
-            series.appendData(DataPoint(1614806485.0,58000.0), false, 8)
-            series.appendData(DataPoint(1614892885.0,55000.0), false, 8)
-            series.appendData(DataPoint(1614989285.0,53000.0), false, 8)
-            series.appendData(DataPoint(1615065696.0,48000.0), false, 8)
-
-
                 series.color = Color.GREEN;
                 series.isDrawDataPoints = true;
                 series.dataPointsRadius = 10F
                 series.thickness = 8;
                 graphCoinPrice.addSeries(series)
-           // graphCoinPrice.gridLabelRenderer.setHorizontalLabelsAngle(90)
-            graphCoinPrice.viewport.isXAxisBoundsManual=true
-            graphCoinPrice.viewport.setMaxX(1615065696.0)
-            graphCoinPrice.viewport.setMinX(1614247285.0)
+                // graphCoinPrice.gridLabelRenderer.setHorizontalLabelsAngle(90)
+                graphCoinPrice.viewport.isXAxisBoundsManual = true
+                graphCoinPrice.viewport.setMinX((it[0].time-160_000).toDouble())
+                graphCoinPrice.viewport.setMaxX((it[6].time+160_000).toDouble())
 
-
-
-
-
-
-          //  }
+            }
 
         })
-        graphCoinPrice.title="За последнюю неделю"
+        graphCoinPrice.title = "За последнюю неделю"
 
         //graphCoinPrice.
 
         graphCoinPrice.gridLabelRenderer.labelFormatter = object : LabelFormatter {
             override fun formatLabel(value: Double, isValueX: Boolean): String {
                 if (isValueX) {
-                    val ff =dataFormat.format(Date(value.toLong()*1000))
-                    return ff
+                    return dataFormat.format(Date(value.toLong() * 1000))
                 }
                 return value.toString()
             }
+
             override fun setViewport(viewport: Viewport?) {
             }
         }
