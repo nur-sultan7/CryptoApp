57f4aa0e85d4ba0a914e35ef362e8159ea2434fb
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-01-06 23:31:50 +0300

    add domain layer

--- app/src/main/java/com/nursultan/cryptoapp/domain/CoinRepository.kt
+++ app/src/main/java/com/nursultan/cryptoapp/domain/CoinRepository.kt
@@ -5,12 +5,8 @@ import com.nursultan.cryptoapp.domain.entity.CoinDailyInfo
 import com.nursultan.cryptoapp.domain.entity.CoinInfo
 
 interface CoinRepository {
-
     fun getCoinInfoListAsc(): LiveData<List<CoinInfo>>
-
     fun getCoinInfoListDesc(): LiveData<List<CoinInfo>>
-
     fun getCoinInfo(): LiveData<CoinInfo>
-
     fun getCoinDailyInfoList(): LiveData<List<CoinDailyInfo>>
 }
