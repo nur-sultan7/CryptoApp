fe382965a7ff2e04cd9bb7318c5b222f16e961f6
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-01-09 03:12:13 +0300

    use view binding in adapter

--- app/src/main/java/com/nursultan/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/nursultan/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -2,19 +2,15 @@ package com.nursultan.cryptoapp.presentation.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.like.LikeButton
 import com.nursultan.cryptoapp.R
+import com.nursultan.cryptoapp.databinding.ItemCoinPriceInfoBinding
 import com.nursultan.cryptoapp.domain.entity.CoinInfo
 import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.item_coin_price_info.view.*
 
 class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoAdapter.CoinInfoViewHolder>() {
+    RecyclerView.Adapter<CoinInfoViewHolder>() {
     var coinPriceInfoList = listOf<CoinInfo>()
 
     private var onCoinClickListener: OnCoinClickListener? = null
@@ -34,31 +30,44 @@ class CoinInfoAdapter(private val context: Context) :
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinInfoViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_coin_price_info, parent, false)
-        return CoinInfoViewHolder(view)
+        val binding = ItemCoinPriceInfoBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CoinInfoViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
         val coinPI = coinPriceInfoList[position]
-        with(holder)
+        with(holder.binding)
         {
             with(coinPI)
             {
-                tvSymbols.text = String.format(
+                tvSymbols10.text = String.format(
                     context.resources.getString(R.string.symbols_template),
                     fromSymbol,
                     toSymbol
                 )
-                tvPrice.text = price.toString().take(8)
-                tvLastUpdate.text = String.format(
+                tvPrice10.text = price.toString().take(8)
+                tvLastUpdateLabel.text = String.format(
                     context.resources.getString(R.string.last_update_template),
                     lastUpdate
                 )
                 btnFav.isLiked = isFav
                 Picasso.get()
                     .load(imageUrl)
-                    .into(ivLogoCoin)
+                    .into(ivLogoCoin10)
+            }
+            root.setOnClickListener {
+                onCoinClickListener?.onClick(coinPriceInfoList[position])
+            }
+            btnFav.setOnClickListener {
+                onFavClickListener?.onClick(
+                    coinPriceInfoList[position],
+                    btnFav.isLiked
+                )
+                btnFav.isLiked = !btnFav.isLiked
             }
         }
 
@@ -66,22 +75,5 @@ class CoinInfoAdapter(private val context: Context) :
 
     override fun getItemCount() = coinPriceInfoList.size
 
-    inner class CoinInfoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvSymbols: TextView = itemView.tvSymbols10
-        val tvPrice: TextView = itemView.tvPrice10
-        val tvLastUpdate: TextView = itemView.tvLastUpdateLabel
-        val ivLogoCoin: ImageView = itemView.ivLogoCoin10
-        val btnFav: LikeButton = itemView.btnFav
-
-        init {
-            itemView.setOnClickListener {
-                onCoinClickListener?.onClick(coinPriceInfoList[absoluteAdapterPosition])
-            }
-            btnFav.setOnClickListener {
-                onFavClickListener?.onClick(coinPriceInfoList[absoluteAdapterPosition], btnFav.isLiked)
-                btnFav.isLiked = !btnFav.isLiked
-            }
-        }
-    }
 
 }
---
+++ app/src/main/java/com/nursultan/cryptoapp/presentation/adapters/CoinInfoViewHolder.kt
@@ -0,0 +1,7 @@
+package com.nursultan.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.RecyclerView
+import com.nursultan.cryptoapp.databinding.ItemCoinPriceInfoBinding
+
+class CoinInfoViewHolder(val binding: ItemCoinPriceInfoBinding) :
+    RecyclerView.ViewHolder(binding.root)
