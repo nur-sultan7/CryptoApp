ba3b02bef5a7dfd8cf2fb1f403f043e102c6f5b5
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-01-31 23:33:29 +0300

    implementation di

--- app/src/main/java/com/nursultan/cryptoapp/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/nursultan/cryptoapp/presentation/ViewModelFactory.kt
@@ -7,7 +7,7 @@ import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
     private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
-) :ViewModelProvider.Factory{
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return viewModelProviders[modelClass]?.get() as T
     }
