0c957b9772e7f7e1b13e22651e268c1315ad1e6a
Nursultan Akhmadov <nur-sultan17@mail.ru>
2022-02-08 13:24:45 +0300

    fix some code

--- app/src/main/java/com/nursultan/cryptoapp/data/database/model/CoinDailyInfoDbModel.kt
+++ app/src/main/java/com/nursultan/cryptoapp/data/database/model/CoinDailyInfoDbModel.kt
@@ -2,8 +2,6 @@ package com.nursultan.cryptoapp.data.database.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "daily_info_data")
 open class CoinDailyInfoDbModel(
